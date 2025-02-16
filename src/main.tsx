1bf22be63e58d7a59444c290725b993a833ed8f8
ayk <leonardo.e.reis1@gmail.com>
2025-02-16 16:33:38 -0300

    minor adjustments, added react-query

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,20 @@ import App from "./App.tsx";
 import Layout from "./layout.tsx";
 import { UserProvider } from "./hooks/user-hooks/user-context.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {" "}
     <BrowserRouter>
-      <UserProvider>
-        <Layout>
-          <App />
-        </Layout>
-      </UserProvider>
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <Layout>
+            <App />
+          </Layout>
+        </UserProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
