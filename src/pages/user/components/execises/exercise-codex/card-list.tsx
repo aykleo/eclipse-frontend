5d36eb48f74e8fdf7b7cbb10cd3dc6f0dab6bee1
ayk <leonardo.e.reis1@gmail.com>
2025-03-28 23:58:34 -0300

    Add new pixel art assets for body components and update CardList styling

--- src/pages/user/components/execises/exercise-codex/card-list.tsx
+++ src/pages/user/components/execises/exercise-codex/card-list.tsx
@@ -52,8 +52,42 @@ export const CardList = memo(
       <div
         className={`${
           isCreatingExercise || exerciseForUpdate ? "hidden" : ""
-        } flex size-full relative bg-stone-950`}
+        } flex size-full relative bg-[#504d4d] mb-10`}
       >
+        <div
+          className="h-full w-4 absolute left-0 bg-black"
+          style={{
+            backgroundImage:
+              "url(src/assets/pixel-art/body/body-side-paper-64.svg)",
+            backgroundRepeat: "repeat-y",
+          }}
+        />
+        <div
+          className="h-[11px] w-1/2 absolute left-1 bottom-0 bg-black"
+          style={{
+            backgroundImage:
+              "url(src/assets/pixel-art/body/body-bottom-paper-48.svg)",
+            backgroundRepeat: "repeat-x",
+          }}
+        />
+
+        <div
+          className="h-full w-4 absolute right-0 bg-black"
+          style={{
+            backgroundImage:
+              "url(src/assets/pixel-art/body/body-side-paper-64.svg)",
+            backgroundRepeat: "repeat-y",
+            transform: "rotateY(180deg)",
+          }}
+        />
+        <div
+          className="h-[11px] w-1/2 absolute right-1 bottom-0 bg-black"
+          style={{
+            backgroundImage:
+              "url(src/assets/pixel-art/body/body-bottom-paper-48.svg)",
+            backgroundRepeat: "repeat-x",
+          }}
+        />
         <div
           className={`${
             isCreatingTemplate ? "w-full lg:w-3/4" : "w-full"
