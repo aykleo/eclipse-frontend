8b7507ada026fb4e92e7c8a9190c623393452109
ayk <leonardo.e.reis1@gmail.com>
2025-04-05 20:26:39 -0300

    Add horizontal category counter and enhance workout card layout
    - Introduced CategoryCounterHorizontal component for improved visualization of exercise category counts in a horizontal layout.
    - Adjusted styling in WorkoutsPage to enhance the layout of workout cards, including increased padding and improved SVG rendering.
    - Updated the rendering of exercise card backgrounds for better visual stacking and alignment.

--- src/pages/workouts/workouts-page.tsx
+++ src/pages/workouts/workouts-page.tsx
@@ -10,6 +10,7 @@ import { CategoryCounts } from "../../components/statistics/exercises/category-c
 import { CategoryCounterVertical } from "../../components/statistics/exercises/category-counter-vertical";
 import { RenderPng } from "../../components/pixel-art/render-png";
 import { RenderSvg } from "../../components/pixel-art/render-svg";
+import { CategoryCounterHorizontal } from "../../components/statistics/exercises/category-counter-horizontal";
 
 export const WorkoutsPage = () => {
   const { user } = useUser() || {};
@@ -71,28 +72,54 @@ export const WorkoutsPage = () => {
           templatesData.templates.map((workout: Template) => (
             <div
               key={workout.id}
-              className="flex flex-col gap-y-2 px-1 bg-neutral-800 py-2 relative"
+              className="flex flex-col gap-y-2 px-1 bg-neutral-800 py-4 relative"
             >
               <RenderSvg
-                src="body/body-t-128.svg"
-                className="absolute top-0 left-0 border w-full h-2"
+                src="body/body-bot-4.svg"
                 size="auto"
                 repeat="repeat-x"
                 position="center"
+                className="absolute left-0 bottom-0 h-1 w-full"
+              />
+
+              <RenderSvg
+                src="body/body-bot-4.svg"
+                size="auto"
+                repeat="repeat-x"
+                position="center"
+                className="absolute left-0 top-0 h-1 w-full"
+                transform="rotate(180deg)"
               />
               <span className="text-2xl font-bold text-white w-full text-center">
                 {workout.name}
               </span>
-              <div className="flex flex-row justify-between">
-                <RenderPng
-                  src="exercise-cards/card-backs/exercise-card-back-1.png"
-                  alt="card-bg"
-                  className=""
-                />
-                <div className="w-full h-48">
+              <div className="flex flex-row justify-between h-max">
+                <div className="relative w-[192px] md:h-[256px]">
+                  <RenderPng
+                    src="exercise-cards/card-backs/exercise-card-back-1.png"
+                    alt="card-bg"
+                    className="-mt-2 absolute top-0 md:top-5"
+                  />
+                  <RenderPng
+                    src="exercise-cards/card-backs/exercise-card-back-1.png"
+                    alt="card-bg"
+                    className="-mt-2 absolute -top-1 md:top-4"
+                  />
+                  <RenderPng
+                    src="exercise-cards/card-backs/exercise-card-back-1.png"
+                    alt="card-bg"
+                    className="-mt-2 absolute -top-2 md:top-3"
+                  />
+                </div>
+                <div className="w-full h-48 md:hidden">
                   <CategoryCounterVertical
                     categoryCounts={calculateCategoryCounts(workout)}
                     hasCount={false}
+                  />
+                </div>
+                <div className="hidden w-full md:grid grid-cols-2">
+                  <CategoryCounterHorizontal
+                    categoryCounts={calculateCategoryCounts(workout)}
                   />
                 </div>
                 {/* {workout.exercises
