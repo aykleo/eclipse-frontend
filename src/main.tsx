9dff1fbf0fddb8865117749155ed6763789a4dda
ayk <leonardo.e.reis1@gmail.com>
2025-04-06 01:42:44 -0300

    Add TemplateProvider to main application structure
    - Integrated TemplateProvider into the main rendering process to manage template-related state.
    - Updated component hierarchy to include TemplateProvider, enhancing the organization of context providers.

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import { UserProvider } from "./hooks/user/user-provider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusProvider } from "./hooks/status/status-provider.tsx";
-
 import { ExerciseProvider } from "./hooks/exercises/exercise-provider.tsx";
+import { TemplateProvider } from "./hooks/templates/template-provider.tsx";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
@@ -18,9 +18,11 @@ createRoot(document.getElementById("root")!).render(
         <StatusProvider>
           <UserProvider>
             <ExerciseProvider>
-              <Layout>
-                <App />
-              </Layout>
+              <TemplateProvider>
+                <Layout>
+                  <App />
+                </Layout>
+              </TemplateProvider>
             </ExerciseProvider>
           </UserProvider>
         </StatusProvider>
