8091c63e0d102ca1c77cee184432efebefcc79d2
ayk <leonardo.e.reis1@gmail.com>
2025-04-10 01:36:02 -0300

    Update layout styles to improve text visibility
    - Changed text color in the layout component to slate-500 for better contrast against the black background, enhancing readability.

--- src/components/exercise/exercise-card.tsx
+++ src/components/exercise/exercise-card.tsx
@@ -12,7 +12,7 @@ export const ExerciseCard = memo(({ exercise }: ExerciseCardProps) => {
     <>
       <RenderPng
         className={`flex-col w-[192px] h-[256px] flex items-center group justify-center transition-all duration-300 relative`}
-        src="exercise-cards/generic-exercise-card.png"
+        src="cards/exercise-card.png"
         alt="exercise-card"
         imgClassName="w-full h-full"
       >
@@ -24,28 +24,28 @@ export const ExerciseCard = memo(({ exercise }: ExerciseCardProps) => {
                 key={muscleGroup.muscleGroup.id}
                 src={`/src/assets/pixel-art/muscles/${muscleGroup.muscleGroup.name}.svg`}
                 alt={`${muscleGroup.muscleGroup.name} icon`}
-                className="size-18"
+                className="size-16 opacity-50"
               />
             ))}
         </div>
 
-        <span className="w-[170px] h-[32px] justify-center px-2.5 absolute top-31.5">
-          <div className="size-full pt-1.5 truncate text-center">
+        <span className="w-[154px] h-[29px] justify-center px-2 absolute top-31">
+          <div className="size-full truncate text-center">
             {exercise.name ? <>{exercise.name}</> : "No name"}
           </div>
         </span>
 
         {exercise.tag.category && (
           <RenderSvg
-            src={`exercise-cards/category-indicator-${exercise.tag.category}.svg`}
+            src={`cards/category-indicator-${exercise.tag.category}.svg`}
             position="center"
             size="auto"
             repeat="no-repeat"
-            className="size-7 rounded-lg p-1 absolute top-28"
+            className="size-7 rounded-lg p-1 absolute top-36"
           />
         )}
 
-        <div className="h-[72px] text-xs p-1 absolute w-[156px] break-words bottom-4 overflow-y-auto no-scrollbar">
+        <div className="h-[84px] text-xxs p-1.5 absolute w-[144px] break-words bottom-2.5 overflow-y-auto no-scrollbar">
           {exercise.description
             ? exercise.description
             : "There is no description"}
--- src/pages/exercises/exercise-codex/new-exercise-btn.tsx
+++ src/pages/exercises/exercise-codex/new-exercise-btn.tsx
@@ -14,17 +14,15 @@ export const NewExerciseBtn = React.memo(() => {
         setExerciseForUpdate(null);
       }}
       className={`w-[192px] h-[256px] relative cursor-pointer flex items-center justify-center flex-grow-0 flex-shrink-0 font-bold opacity-50 hover:opacity-100`}
-      src="exercise-cards/new-exercise-card.png"
+      src="cards/new-exercise-card.png"
       alt="new-exercise-card"
       imgClassName="absolute top-0 left-0 w-full h-full"
     >
-      <span className="w-[170px] h-[32px] justify-center px-2.5 absolute top-31.5">
-        <div className="size-full pt-1.5 truncate text-center">
-          New exercise
-        </div>
+      <span className="w-[154px] h-[29px] justify-center px-2 absolute top-31">
+        <div className="size-full truncate text-center">New exercise</div>
       </span>
 
-      <div className="h-[72px] text-xs p-1 absolute w-[156px] bottom-4 overflow-y-auto no-scrollbar">
+      <div className="h-[84px] text-xxs p-1.5 absolute w-[144px] break-words bottom-2.5 overflow-y-auto no-scrollbar">
         Activate to create a new exercise
       </div>
     </RenderPng>
